b3a045baaac01ba20760c5b00f2c9f6847134f55
Juraj Jancuska <jjancuska@gmail.com>
2023-11-07 21:53:49 +0100

    Remove dependency on 'templating' service, to be compatible with Symfony 5

--- src/Controller/SortingController.php
+++ src/Controller/SortingController.php
@@ -18,10 +18,11 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Templating\EngineInterface;
 use Symfony\Component\Translation\TranslatorInterface;
+use Twig\Environment;
 
 class SortingController
 {
-	/** @var EngineInterface */
+	/** @var Environment */
 	private $templatingEngine;
 	/**
 	 * @var TaxonRepositoryInterface
@@ -53,7 +54,7 @@ class SortingController
 	private $translator;
 
 	public function __construct(
-		EngineInterface $templatingEngine,
+		Environment $templatingEngine,
 		TaxonRepositoryInterface $taxonRepository,
 		ProductTaxonRepositoryInterface $productTaxonRepository,
 		EntityManagerInterface $entityManager,
